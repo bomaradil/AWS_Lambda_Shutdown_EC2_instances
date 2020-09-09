822fb33274968ebd696c2fe8a12d854f4396cef7
bomaradil <56771009+bomaradil@users.noreply.github.com>
2020-09-09 20:27:52 +0100

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,30 +1,45 @@
-#shutdown Ec2 instances
+# Shutdown Ec2 instances using AWS Lambda and python code
+#This Python code will shutdown running or pending ec2 instances in that region 
+
+#import the aws library boto3
 import boto3
 
 def Get_Running_Instances():
     """
     fonction to get ec2 instances that are in state Running or pending
-    return a list of all the instance filter
+    and return a list with all the instances id
     """
-    ec2 = boto3.resource('ec2')
+    ec2 = boto3.resource('ec2') 
+    #call the features resource from the boto3 library
     instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['pending', 'running',]},])
+    #filter the instances returned using the state name
     #you can also filter using Tags by adding the filters: 
     #[{'Name': 'tag-key', 'Values': ['Role','Name',]}, {'Name': 'tag-value', 'Values': ['*test*', '*TEST*',]},]
     return [instance.id for instance in instances]
+    #return a liste with the ids of the instances
     
 def Stop_Instances(ids=Get_Running_Instances()):
     """
     shutdown the Ec2 instances that has been returned by the fonction Get_Running_Instances
     """
     ec2 = boto3.client('ec2')
+    #call the features client from the boto3 library
     if not ids:
-        #if the list of Ec2 instances is empty no action needed
+        #if the list of Ec2 instances returned is empty.
         print("No Instance in the state Running or pending")
     else:
         ec2.stop_instances(InstanceIds=ids)
+        #stop the instances using their id
         ec2.get_waiter('instance_stopped').wait(InstanceIds=ids)
-        #wait for the instances to have the state stopped.
+        #wait for the state of the instances to change to stopped.
         print('instance {} was shutdown'.format(ids))
 
 def lambda_handler(event, context):
+    """
+    launch the fonction Stop_Instances() in the lambda function 
+    Handler for the Lambda function "lambda_function.lambda_handler"
+    Timeout need to be more than 1min, so that our function can run perfectly 
+    if you have an important number of instances to be shutdown, change the parameter of timeout
+    """
     Stop_Instances()
+
